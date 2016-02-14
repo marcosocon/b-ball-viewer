38f89fb7309c6ad7917357be2117a87f3c28dfcf
marcosocon <oconmarcos97@gmail.com>
2016-02-14 16:50:25 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var store = angular.module('bball', []);
+app.controller('StoreController', function(){
+  this.product = balls;
+})
+var balls = [
+{
+  name: "ball",
+  description : "a good ball",
+  price : "123"
+},
+{
+  name: "balls",
+  description: "a bad ball",
+  price:"122"
+}
+]
