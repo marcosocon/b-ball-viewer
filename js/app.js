7b670358d72b2e28d9ac965c3365b048441412ef
Marcos Ocon <oconmarcos97@gmail.com>
2016-02-15 10:41:12 -0300

    added panel controller and pills for display description,specs and reviews

--- js/app.js
+++ js/app.js
@@ -47,4 +47,14 @@ var balls =
   },
 ];
 
+app.controller('PanelController', function(){
+    this.tab = 1;
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
 })();
