1c9c391f7c3c801cd4bb793168ff68fdbdece124
Marcos Ocon <oconmarcos97@gmail.com>
2016-02-15 10:51:49 -0300

    added specs to b-balls

--- js/app.js
+++ js/app.js
@@ -8,41 +8,74 @@
 var balls =
 [
   { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
     price: 50.50
   },
   {
   name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
   price: 40.90
   },
   { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
     price: 50.50
   },
   {
   name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
   price: 40.90
   },
   { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
     price: 50.50
   },
   {
   name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
   price: 40.90
   },
   { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
     price: 50.50
   },
   {
   name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
   price: 40.90
   },
   { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
     price: 50.50
   },
   {
   name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
   price: 40.90
   },
   { name: 'Nassau',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews: 'None yet!',
   price: 34.33
   },
 ];
