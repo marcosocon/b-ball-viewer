93c2597a7cc22a71b33225aee1aa953a228fcf55
Marcos Ocon <oconmarcos97@gmail.com>
2016-02-16 14:19:29 -0300

    added directives and refactorized code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,80 @@
 (function() {
-  var app = angular.module('bball', []);
 
-  app.controller('StoreController', function(){
-    this.products = balls;
-  });
+
+// Initializer.
+
+var app = angular.module('bball', []);
+
+
+// Controllers.
+
+
+app.controller('StoreController', function(){
+  this.products = balls;
+});
+
+app.controller('PanelController', function(){
+  this.tab = 1;
+  this.selectTab = function(setTab){
+    this.tab = setTab;
+  };
+  this.isSelected = function(checkTab){
+    return this.tab === checkTab;
+  };
+});
+
+app.controller('ReviewController', function(){
+  this.review = {};
+  this.addReview = function(product){
+    product.reviews.push(this.review);
+    this.review = {};
+  };
+});
+
+
+// Directives.
+
+
+app.directive('productTitle', function(){
+  return{
+    restrict:'E',
+    templateUrl: 'product-title.html'
+  };
+});
+app.directive('productDescription', function(){
+  return{
+    restrict:'E',
+    templateUrl: 'product-description.html'
+  };
+});
+app.directive('productSpecs', function(){
+  return{
+    restrict:'E',
+    templateUrl: 'product-specs.html'
+  };
+});
+app.directive('productReview', function(){
+  return{
+    restrict:'E',
+    templateUrl: 'product-review.html'
+  };
+});
+app.directive('productPanels', function(){
+  return{
+    restrict:"E",
+    templateUrl:"product-panels.html",
+    controller:function(){
+    this.tab = 1;
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+      };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  },
+    controllerAs: 'panel'
+};
+});
 
 var balls =
 [
@@ -80,20 +151,4 @@ var balls =
   },
 ];
 
-app.controller('PanelController', function(){
-    this.tab = 1;
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    };
-  });
-app.controller('ReviewController', function(){
-  this.review = {};
-  this.addReview = function(product){
-    product.reviews.push(this.review);
-    this.review = {};
-  };
-});
 })();
