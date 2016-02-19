e4d5b23cd79c80fb8d4861de42a4a2fb8aa3b777
Marcos Ocon <oconmarcos97@gmail.com>
2016-02-19 12:50:31 -0300

    added images gallery, final commit

--- js/app.js
+++ js/app.js
@@ -8,14 +8,9 @@ var app = angular.module('bball', ['store-products']);
 
 // Controllers.
 
-
-app.controller('StoreController', ['$http', function($http){
-  var store = this;
-  store.products = [];
-  $http.get('/store-products.json').then(function(data){
-    store.products = data;
+  app.controller('StoreController', function(){
+    this.products = balls;
   });
-}]);
 
 
 app.controller('PanelController', function(){
@@ -35,5 +30,91 @@ app.controller('ReviewController', function(){
     this.review = {};
   };
 });
+
+var balls =
+[
+  { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://www.cityofparkland.org/ImageRepository/Document?documentID=9270',
+    price: 50.50
+  },
+  {
+  name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/1UoAAMXQydtTNYPP/$_57.JPG',
+    price: 40.90
+  },
+  { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://www.cityofparkland.org/ImageRepository/Document?documentID=9270',
+    price: 50.50
+  },
+  {
+  name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/1UoAAMXQydtTNYPP/$_57.JPG',
+  price: 40.90
+  },
+  { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://www.cityofparkland.org/ImageRepository/Document?documentID=9270',
+    price: 50.50
+  },
+  {
+  name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/1UoAAMXQydtTNYPP/$_57.JPG',
+  price: 40.90
+  },
+  { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://www.cityofparkland.org/ImageRepository/Document?documentID=9270',
+    price: 50.50
+  },
+  {
+  name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/1UoAAMXQydtTNYPP/$_57.JPG',
+  price: 40.90
+  },
+  { name: 'Spalding',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://www.cityofparkland.org/ImageRepository/Document?documentID=9270',
+    price: 50.50
+  },
+  {
+  name: 'Molten',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://i.ebayimg.com/00/s/MTYwMFgxNjAw/z/1UoAAMXQydtTNYPP/$_57.JPG',
+  price: 40.90
+  },
+  { name: 'Nassau',
+    description: 'lorem inpdasn asdkmas kmasdnf najs lorem dsasmdk mkmsd aksm namsdasdmawm kamsdkawkadmasmdkawmk',
+    specs: 'ball with 1.25 pull inches and great movement with grass and concret',
+    reviews:[],
+    image: 'http://www.cityofparkland.org/ImageRepository/Document?documentID=9270',
+  price: 34.33
+  },
+];
 
 })();
